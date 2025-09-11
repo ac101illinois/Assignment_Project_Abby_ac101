beb95f99df073eddfa90c7dd239d688273a2f115
Abby Circelli <ac101@illinois.edu>
2025-09-11 18:09:24 -0500

    last commit before running

--- CoffeeLocationProject/asgi.py
+++ CoffeeLocationProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Assignment_Project_Abby_ac101 project.
+ASGI config for CoffeeLocationProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment_Project_Abby_ac101.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoffeeLocationProject.settings')
 
 application = get_asgi_application()
--- CoffeeLocationProject/urls.py
+++ CoffeeLocationProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Assignment_Project_Abby_ac101 project.
+URL configuration for CoffeeLocationProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
