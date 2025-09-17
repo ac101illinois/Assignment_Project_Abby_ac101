b38939ea91e5fb2643aee9d954d3b01422e38165
Abby Circelli <ac101@illinois.edu>
2025-09-17 14:27:16 -0500

    Registered tables to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, CoffeeShop, Visit, Review
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(CoffeeShop)
+admin.site.register(Visit)
+admin.site.register(Review)
