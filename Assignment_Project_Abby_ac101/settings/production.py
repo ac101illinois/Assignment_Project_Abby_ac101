fa11e49598369e52a188ff4514a43fbedb543e9d
Abby Circelli <ac101@illinois.edu>
2025-11-18 13:50:16 -0600

    Up to date

--- Assignment_Project_Abby_ac101/settings/production.py
+++ Assignment_Project_Abby_ac101/settings/production.py
@@ -1,1 +1,7 @@
 from .base import *
+
+DEBUG = False
+
+# Enter your PythonAnywhere account here in the allowed hosts.
+# Replace it with your name:
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ac101.pythonanywhere.com']
